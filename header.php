1a3ed0fd48655416f14ca661e6d71c3bd49c8aae
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2023-05-29 19:47:47 +0545

    Test completed

--- header.php
+++ header.php
@@ -17,8 +17,6 @@
           <li><a href="#" >Contacts</a></li>
           <li><a href="signup.php" >Signup</a></li>
           <li><a href="signin.php" >Login</a></li>
-          <li><a href="">Trial</a></li>
-          <li>haha my name is sharad</li>
         </ul>
       </div>
 </body>
