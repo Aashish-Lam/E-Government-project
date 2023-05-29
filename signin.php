343230d8d22e04c40ffa51dcfec3f751194363f2
Sharad <sharadritu.58.69@gmail.com>
2023-05-29 22:39:42 +0545

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,3 +1,36 @@
+<?php
+
+include 'connection.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+    $user_id = $_SESSION['user_id'];
+}else{
+    $user_id = '';
+};
+
+if(isset($_POST['submit'])){
+
+    $email = $_POST['email'];
+    $email = filter_var($email, FILTER_UNSAFE_RAW);
+    $pass = sha1($_POST['pass']);
+    $pass = filter_var($pass, FILTER_UNSAFE_RAW);
+
+    $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ?");
+    $select_user->execute([$email, $pass]);
+    $row = $select_user->fetch(PDO::FETCH_ASSOC);
+
+    if($select_user->rowCount() > 0){
+        $_SESSION['user_id'] = $row['id'];
+        header('location:index.php');
+    }else{
+        $message[] = 'Incorrect Email or Password!';
+    }
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +39,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> Sign In Form </title>
-  <link rel="stylesheet" type="text/css" href="signin.css">
+  <link rel="stylesheet" type="text/css" href="sign.css">
   <script src="blob.js"></script>
 </head> 
 
@@ -15,14 +48,14 @@
   include_once 'header.php'
   ?>
 
-  <form name="myForm" action="signin.php" method="POST">
+  <form name="myForm" action="" method="POST">
     <div class="login-box">
 
       <h1> Sign In </h1>
 
       <div class="textbox">
 
-        <input type="text" placeholder="Username" name="user" value="" required>
+        <input type="email" placeholder="Email" name="email" value="" required>
 
       </div>
       <div class="textbox">
