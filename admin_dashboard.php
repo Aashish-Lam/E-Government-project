7d917e171498d9178b216eae699b0cd53999bc2a
Sharad <sharadritu.58.69@gmail.com>
2023-06-01 17:52:00 +0545

    changes to admin dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -2,7 +2,13 @@
 
 include 'connection.php';
 session_start();
-echo $_SESSION['admin_id'];
+
+if(isset($_SESSION['admin_id'])){
+    $admin_id = $_SESSION['admin_id'];
+}else{
+    $admin_id = '';
+};
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,8 +17,87 @@ echo $_SESSION['admin_id'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="admin.css">
 </head>
 <body>
-    <h1>This is admin page</h1>
+    <?php
+        include 'admin_header.php';
+    ?>
+
+    <section class="dashboard">
+        <div class="box-container">
+            <div class="box">
+                <?php
+                    $select_users = $conn->prepare("SELECT * FROM `users`");
+                    $select_users->execute();
+                    $number_of_users = $select_users->rowCount()
+                ?>
+                <h3><?= $number_of_users; ?></h3>
+                <p>Users</p>
+                <a href="see_users.php" class="btn">View List</a>
+            </div>
+
+            <div class="box">
+                <?php
+                    $select_u_certificates = $conn->prepare("SELECT * FROM `u_certificates`");
+                    $select_u_certificates->execute();
+                    $number_of_u_certificates = $select_u_certificates->rowCount()
+                ?>
+                <h3><?= $number_of_u_certificates; ?></h3>
+                <p>Unapproved Certificates</p>
+                <a href="see_u_certificates.php" class="btn">View List</a>
+            </div>
+
+            <div class="box">
+                <?php
+                    $select_a_certificates = $conn->prepare("SELECT * FROM `a_certificates`");
+                    $select_a_certificates->execute();
+                    $number_of_a_certificates = $select_a_certificates->rowCount()
+                ?>
+                <h3><?= $number_of_a_certificates; ?></h3>
+                <p>Approved Certificates</p>
+                <a href="see_a_certificates.php" class="btn">View List</a>
+            </div>
+
+            <div class="box">
+                <?php
+                    $select_admin = $conn->prepare("SELECT * FROM `admin`");
+                    $select_admin->execute();
+                    $number_of_admin = $select_admin->rowCount()
+                ?>
+                <h3><?= $number_of_admin; ?></h3>
+                <p>Admin</p>
+                <a href="see_users.php" class="btn">View List</a>
+            </div>
+
+            <div class="box">
+                <?php
+                    $select_users = $conn->prepare("SELECT * FROM `users`");
+                    $select_users->execute();
+                    $number_of_users = $select_users->rowCount()
+                ?>
+                <h3><?= $number_of_users; ?></h3>
+                <p>Feedback</p>
+                <a href="see_users.php" class="btn">View List</a>
+            </div>
+
+            <div class="box">
+                <?php
+                    $select_users = $conn->prepare("SELECT * FROM `users`");
+                    $select_users->execute();
+                    $number_of_users = $select_users->rowCount()
+                ?>
+                <h3><?= $number_of_users; ?></h3>
+                <p>Users</p>
+                <a href="see_users.php" class="btn">View List</a>
+            </div>
+        </div>
+    </section>
+
+    <?php
+        include 'footer.php';
+    ?>
+    <script src="script.js"></script>
 </body>
 </html>
