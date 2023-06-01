4e08394e3093cc6a8e7e1b42cac8a83917a61b2d
Sharad <sharadritu.58.69@gmail.com>
2023-06-01 13:48:53 +0545

    adding about page

--- certificate.php
+++ certificate.php
@@ -21,7 +21,7 @@ $select_certificates->execute([$user_id]);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Birth Certficates</title>
 
     <link rel="stylesheet" href="certificate.css">
 </head>
@@ -62,10 +62,6 @@ $select_certificates->execute([$user_id]);
     }
     ?>
 </ul>
-
-<?php
-include 'footer.php';
-?>
 
 <script src="script.js"></script>
 </body>
