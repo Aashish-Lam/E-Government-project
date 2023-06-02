2f9ec4b79d9f185502e58516652bd8eac2badd7e
Sharad <sharadritu.58.69@gmail.com>
2023-06-02 07:28:46 +0545

    ggjh

--- birth_form.php
+++ birth_form.php
@@ -105,7 +105,7 @@ if(isset($_POST['submit'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Birth Registration Form</title>
 
-    <link rel="stylesheet" href="birth_form.css">
+    <link rel="stylesheet" href="birth_form2.css">
 </head>
 <body>
 
